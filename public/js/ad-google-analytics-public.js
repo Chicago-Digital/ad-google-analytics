deb3ca082c92e4866c08cda775cc4f12b2b89607
scottshefler <3912950+scottshefler@users.noreply.github.com>
2020-05-08 13:06:11 -0500

    small code tweaks to ad-google-analytics-public.js

--- public/js/ad-google-analytics-public.js
+++ public/js/ad-google-analytics-public.js
@@ -3,11 +3,6 @@
 
   function aga_send_event(category, action, label, event_delay) {
 
-		//console.log(category);
-		//console.log(action);
-		//console.log(label);
-		//console.log(event_delay);
-
     if (event_delay) {
 
       var aga_redirect_link = function() {
@@ -47,10 +42,10 @@
         aga_send_event(this.getAttribute("data-ga-category") || "telephone", this.getAttribute("data-ga-action") || "call", this.getAttribute("data-ga-label") || this.href);
       } else if (this.href.indexOf(aga_event_data.options.root_domain) == -1 && (e.target.protocol === "http:" || e.target.protocol === "https:")) {
         if (this.target != "_blank") {
-          aga_send_event(this.getAttribute("data-ga-category") || "outbound", this.getAttribute("data-vars-ga-action") || "click", this.getAttribute("data-vars-ga-label") || this.href, true);
+          aga_send_event(this.getAttribute("data-ga-category") || "outbound", this.getAttribute("data-ga-action") || "click", this.getAttribute("data-ga-label") || this.href, true);
           e.preventDefault();
         } else {
-          aga_send_event(this.getAttribute("data-ga-category") || "outbound", this.getAttribute("data-vars-ga-action") || "click", this.getAttribute("data-vars-ga-label") || this.href);
+          aga_send_event(this.getAttribute("data-ga-category") || "outbound", this.getAttribute("data-ga-action") || "click", this.getAttribute("data-ga-label") || this.href);
         }
       } else if (this.href.match(link_match)) {
         aga_send_event(this.getAttribute("data-ga-category") || "download", this.getAttribute("data-ga-action") || "click", this.getAttribute("data-ga-label") || this.href);
