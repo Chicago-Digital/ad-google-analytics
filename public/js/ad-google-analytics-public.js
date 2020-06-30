f847944b2550bf8fd249ed1d4a7f87ec3dc2be9d
scottshefler <3912950+scottshefler@users.noreply.github.com>
2020-06-29 21:23:18 -0500

    updating to fix outbound links issue

--- public/js/ad-google-analytics-public.js
+++ public/js/ad-google-analytics-public.js
@@ -1,28 +1,12 @@
 (function($) {
   'use strict';
 
-  function aga_send_event(category, action, label, event_delay) {
+  function aga_send_event(category, action, label) {
 
-    if (event_delay) {
-
-      var aga_redirect_link = function() {
-        window.location.href = label;
-      };
-
-      gtag("event", action, {
-        'event_category': category,
-        'event_label': label,
-        'event_callback': aga_redirect_link
-      });
-
-      setTimeout(aga_redirect_link, 150);
-
-    } else {
-      gtag("event", action, {
-        'event_category': category,
-        'event_label': label
-      });
-    }
+    gtag("event", action, {
+      'event_category': category,
+      'event_label': label
+    });
 
   }
 
@@ -34,23 +18,26 @@
 
     // Download Link Match
     var link_match = new RegExp(".*\\.(" + aga_event_data.options.event_downloads + ")(\\?.*)?$");
+
     // Link Click
     $(document).on("click", "a", function(e) {
+      // Email Links
       if (e.target.protocol === "mailto:") {
         aga_send_event(this.getAttribute("data-ga-category") || "email", this.getAttribute("data-ga-action") || "send", this.getAttribute("data-ga-label") || this.href);
+      // Telephone Links
       } else if (e.target.protocol === "tel:") {
         aga_send_event(this.getAttribute("data-ga-category") || "telephone", this.getAttribute("data-ga-action") || "call", this.getAttribute("data-ga-label") || this.href);
-      } else if (this.href.indexOf(aga_event_data.options.root_domain) == -1 && (e.target.protocol === "http:" || e.target.protocol === "https:")) {
-        if (this.target != "_blank") {
-          aga_send_event(this.getAttribute("data-ga-category") || "outbound", this.getAttribute("data-ga-action") || "click", this.getAttribute("data-ga-label") || this.href, true);
-          e.preventDefault();
-        } else {
-          aga_send_event(this.getAttribute("data-ga-category") || "outbound", this.getAttribute("data-ga-action") || "click", this.getAttribute("data-ga-label") || this.href);
-        }
+      // External Links Opening in New Tab/Window.
+      // NOTE: Excluding links not opening in new tab due to limiations of
+      // having to stop default link action, send event and then trigger redirect issues
+      } else if (this.href.indexOf(aga_event_data.options.root_domain) == -1 && (e.target.protocol === "http:" || e.target.protocol === "https:") && this.target === "_blank") {
+        aga_send_event(this.getAttribute("data-ga-category") || "outbound", this.getAttribute("data-ga-action") || "click", this.getAttribute("data-ga-label") || this.href);
+      // Download Links
       } else if (this.href.match(link_match)) {
         aga_send_event(this.getAttribute("data-ga-category") || "download", this.getAttribute("data-ga-action") || "click", this.getAttribute("data-ga-label") || this.href);
       }
     });
+
   });
 
 })(jQuery);
